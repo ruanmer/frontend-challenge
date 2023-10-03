3e7ad80beb79b1eb5b64c183efe65e307fec1b93
Ruan Mer <ruanrm@gmail.com>
2023-10-03 03:21:03 -0300

    feat(Panel): add prop loading

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Panel.module.css";
+import Loader from "../Loader";
 
-const Panel = ({ children }) => {
+const Panel = (props) => {
+  const { children, loading = false } = props;
+
   return (
     <div className={styles.Wrapper}>
-      <div className={styles.Panel}>{children}</div>
+      <div className={styles.Panel}>{loading ? <Loader /> : children}</div>
     </div>
   );
 };
 
 Panel.propTypes = {
   children: PropTypes.node,
+  loading: PropTypes.bool,
 };
 
 export default Panel;
